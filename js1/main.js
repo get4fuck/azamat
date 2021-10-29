24f6f42044c0e5be5e13ff5c03406aa11254932c
get4fuck <91108410+get4fuck@users.noreply.github.com>
2021-10-29 10:10:03 +0300

    Add files via upload

---
+++ js1/main.js
@@ -0,0 +1,16 @@
+var counter=0;
+
+function onClickButton(el) {
+     counter++;
+     el.innerHTML="Вы нажали на кнопку" +counter;
+
+     el.style.cssText="border-radius: 5px; border: 0; fonr-size: 20px";
+}
+ 
+function onInput(el) {
+
+    if(el.value=="Hello")
+     alert("И тебе привет!");
+
+    console.log(el.value);
+}
